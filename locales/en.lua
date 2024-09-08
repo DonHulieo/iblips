99adfce285ba889fba04f7659d64e00032173421
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2024-09-08 16:42:17 +1000

    chore(shared): Comment for Release

--- locales/en.lua
+++ locales/en.lua
@@ -1,37 +1,37 @@
 return {
   ['AU'] = {
     ['Jobs'] = {
-      {
-        name = 'test',
-        creator = {
-          title = 'Test',
-          info = {
-            {
-              title = 'Title Test'
-            },
-            {
-              title = 'Title_Text Title Test',
-              text = 'Title_Text Text Test'
-            },
-            {
-              title = 'Icon Title Test',
-              text = 'Icon Text Test'
-            },
-            {
-              title = 'Player Title Test',
-              text = 'DonHulieo',
-              crew = 'ECIK',
-            },
-            {
-              title = 'Header Title Test',
-              text = 'Header Text Test'
-            },
-            {
-              text = 'Just Text Test'
-            }
-          }
-        }
-      }
+      -- {
+      --   name = 'test',
+      --   creator = {
+      --     title = 'Test',
+      --     info = {
+      --       {
+      --         title = 'Title Test'
+      --       },
+      --       {
+      --         title = 'Title_Text Title Test',
+      --         text = 'Title_Text Text Test'
+      --       },
+      --       {
+      --         title = 'Icon Title Test',
+      --         text = 'Icon Text Test'
+      --       },
+      --       {
+      --         title = 'Player Title Test',
+      --         text = 'DonHulieo',
+      --         crew = 'ECIK',
+      --       },
+      --       {
+      --         title = 'Header Title Test',
+      --         text = 'Header Text Test'
+      --       },
+      --       {
+      --         text = 'Just Text Test'
+      --       }
+      --     }
+      --   }
+      -- }
     }
   },
   ['US'] = {
--- shared/config.lua
+++ shared/config.lua
@@ -3,91 +3,91 @@ local ivec3 = ivec3 --[[@as fun(x: number, y: number, z: number): vector3]] -- A
 return {
   ---@type {['Jobs']: {type: BLIP_TYPES, data: {coords: vector3|vector4?, width: number?, height: number?, radius: number?}, options: blip_options, creator: blip_creator_options}[]}
   ['Jobs'] =  {
-    {
-      type = 'coord',
-      data = {coords = vector3(-74.735076904297, -2033.3594970703, 15.7)},
-      options = {
-        name = 'Jobs.1.name',
-        colours = {
-          opacity = 150,
-          primary = 1
-        },
-        display = {
-          display = 'all_select',
-          priority = 2
-        },
-        flashes = {
-          enable = true,
-          interval = 500,
-          colour = 12
-        },
-        style = {
-          sprite = 1,
-          scale = 2.0,
-          discoverable = false,
-          friendly = false,
-          bright = false,
-          hidden = false,
-          high_detail = false,
-          show_cone = false,
-          short_range = true,
-          shrink = false
-        },
-        indicators = {
-          crew = false,
-          friend = false,
-          completed = false,
-          heading = false,
-          height = false,
-          count = false,
-          outline = false,
-          tick = false
-        },
-        distance = 250.0
-      },
-      creator = {
-        title = 'Jobs.1.creator.title',
-        verified = 'none',
-        rp = '1000',
-        money = '56',
-        ap = '100',
-        info = {
-          {
-            title = 'Jobs.1.creator.info.1.title',
-            type = 0
-          },
-          {
-            title = 'Jobs.1.creator.info.2.title',
-            text = 'Jobs.1.creator.info.2.text',
-            type = 1
-          },
-          {
-            title = 'Jobs.1.creator.info.3.title',
-            text = 'Jobs.1.creator.info.3.text',
-            icon = 1,
-            colour = 12,
-            checked = true,
-            type = 2
-          },
-          {
-            title = 'Jobs.1.creator.info.4.title',
-            text = 'Jobs.1.creator.info.4.text',
-            crew = 'Jobs.1.creator.info.4.crew',
-            is_social_club = false,
-            type = 3
-          },
-          {
-            title = 'Jobs.1.creator.info.5.title',
-            text = 'Jobs.1.creator.info.5.text',
-            type = 4
-          },
-          {
-            text = 'Jobs.1.creator.info.6.text',
-            type = 5
-          }
-        }
-      }
-    },
+    -- {
+    --   type = 'coord',
+    --   data = {coords = vector3(-74.735076904297, -2033.3594970703, 15.7)},
+    --   options = {
+    --     name = 'Jobs.1.name',
+    --     colours = {
+    --       opacity = 150,
+    --       primary = 1
+    --     },
+    --     display = {
+    --       display = 'all_select',
+    --       priority = 2
+    --     },
+    --     flashes = {
+    --       enable = true,
+    --       interval = 500,
+    --       colour = 12
+    --     },
+    --     style = {
+    --       sprite = 1,
+    --       scale = 2.0,
+    --       discoverable = false,
+    --       friendly = false,
+    --       bright = false,
+    --       hidden = false,
+    --       high_detail = false,
+    --       show_cone = false,
+    --       short_range = true,
+    --       shrink = false
+    --     },
+    --     indicators = {
+    --       crew = false,
+    --       friend = false,
+    --       completed = false,
+    --       heading = false,
+    --       height = false,
+    --       count = false,
+    --       outline = false,
+    --       tick = false
+    --     },
+    --     distance = 250.0
+    --   },
+    --   creator = {
+    --     title = 'Jobs.1.creator.title',
+    --     verified = 'none',
+    --     rp = '1000',
+    --     money = '56',
+    --     ap = '100',
+    --     info = {
+    --       {
+    --         title = 'Jobs.1.creator.info.1.title',
+    --         type = 0
+    --       },
+    --       {
+    --         title = 'Jobs.1.creator.info.2.title',
+    --         text = 'Jobs.1.creator.info.2.text',
+    --         type = 1
+    --       },
+    --       {
+    --         title = 'Jobs.1.creator.info.3.title',
+    --         text = 'Jobs.1.creator.info.3.text',
+    --         icon = 1,
+    --         colour = 12,
+    --         checked = true,
+    --         type = 2
+    --       },
+    --       {
+    --         title = 'Jobs.1.creator.info.4.title',
+    --         text = 'Jobs.1.creator.info.4.text',
+    --         crew = 'Jobs.1.creator.info.4.crew',
+    --         is_social_club = false,
+    --         type = 3
+    --       },
+    --       {
+    --         title = 'Jobs.1.creator.info.5.title',
+    --         text = 'Jobs.1.creator.info.5.text',
+    --         type = 4
+    --       },
+    --       {
+    --         text = 'Jobs.1.creator.info.6.text',
+    --         type = 5
+    --       }
+    --     }
+    --   }
+    -- },
   },
   ---@type {['Mission']: {type: BLIP_TYPES, data: {coords: vector3|vector4?, width: number?, height: number?, radius: number?}, options: blip_options, creator: blip_creator_options}[]}
   ['Mission'] = {},
